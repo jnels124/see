43c240e14b7254e2c3b83549a69e80b7ee148904
Jesse Nelson <jnels124@msudenver.edu>
2013-05-04 16:49:05 -0600

    Updated Documentation

--- src/Plays.java
+++ src/Plays.java
@@ -1,12 +1,27 @@
+import java.util.*;
+import java.io.*;
+
+/* This class will drive the lotto analyzation program. It will simulate 
+     several strategies for playing the lottery.
+ 
+   @author Jesse Nelson
+   @author Josh Gillham
+   @version May 04, 2013 : OS:3.8.10-1-ARCH : JavaVersion:ArchLinux build 7.u21_2.3.9-1-x86_64 
+ */
 public class Plays {
     private static List<LottoGame> lottoHistory;
+    private static List<Strategy> playedStrategies; // Add your strategies to this list
     public static void main( String[] args ) {
 	lottoHistory = storeLottoHistory ( args [0] );
         System.out.print( args[0] );
     }
 
+    /* Read lottery data from file specified on command line
+       @param fileName Name of file holding lottery data
+       @return games A list of lottoGame objects created from lottery history 
+     */
     private static List<LottoGame> storeLottoHistory ( String fileName ) {
-	ArrayList<LottoGame> games = new ArrayList<Integer> ( );
+	ArrayList<LottoGame> games = new ArrayList<LottoGame> ( );
 	ArrayList<Integer> gameNumbers;
 	StringTokenizer st;
 	String line;
@@ -21,11 +36,24 @@ public class Plays {
 		while ( st.hasMoreTokens ( ) ) { // Incase we have data formatted differently
 		    Date drawingDate = new Date ( st.nextToken ( ) );
 		    for ( int i = 0; i < 5; i++ ) {
-			gameNumbers.add ( Integer.parseInteger ( st.nextToken ) ); 
+			gameNumbers.add ( Integer.parseInt ( st.nextToken ( ) ) ); 
 		    }
-		    games.add ( new LottoGame ( drawingDate, gameNumbers, null ) ); 
+		    games.add ( new LottoGame ( drawingDate, gameNumbers, 0.0 ) ); 
 		}
 	    }
 	}
+	catch ( FileNotFoundException e ) {
+	    System.out.println ( e );
+	}
+
+	/*catch ( IOException e ) {
+	    System.out.println ( e );
+	    }*/
+	
+	catch ( NumberFormatException e ) {
+	    System.out.println ( e );
+	}
+	
+	return games;
     }
 }
--- src/Strategies.java
+++
@@ -1,6 +0,0 @@
-public interface Strategies {
-	public static void play ( );
-	public static void analyze ( );
-	public static void printResults ( );
-	public static void graphData ( );
-}
