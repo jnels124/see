40c91ba1cecf28d648a85a5cd07cff8a5e7a2ed7
Jesse Nelson <jnels124@msudenver.edu>
2013-05-04 17:08:01 -0600

    Added Documentation

--- src/LottoGame.java
+++ src/LottoGame.java
@@ -1,5 +1,10 @@
 import java.util.*;
-// This class will hold the data for a lotto drawing;
+/* This class will hold the data for a lotto drawing;
+   @author Jesse Nelson
+   @author Josh Gillham
+   @version May 04, 2013 : OS:3.8.10-1-ARCH : JavaVersion:ArchLinux build 7.u21_2.3.9-1-x86_64 
+ */
+
 public class LottoGame {
     private final Date drawingDate;
     private final List <Integer> winningNumbers;
@@ -21,14 +26,26 @@ public class LottoGame {
 	return nums;
 	}*/
 
+    /* This method gets the drawing date for a game
+     * 
+     * @return drawingDate
+     */
     public Date getDrawingDate ( ) {
 	return this.drawingDate;
     }
-
+    
+    /* This method gets the winning numbers for a game
+     * 
+     * @return winngingNumbers
+     */
     public List<Integer> getWinningNumbers ( ) {
 	return this.winningNumbers;
     }
 
+    /* This method gets the jackpot for a game
+     * 
+     * @return jackpot
+     */
     public double getJackpot ( ) { 
 	return this.jackpot;
     }
