2efb8b821574889167d1fc8e624b32d30db712b4
Jesse Nelson <jnels124@msudenver.edu>
2013-05-04 20:07:07 -0600

    Added Hit.java from upstream

---
+++ src/Hit.java
@@ -0,0 +1,11 @@
+import java.util.Date;
+/**
+* Holds the count of winners from a drawing date.
+*
+* @author Josh Gillham
+* @version 5-4-13
+*/
+public class Hit extends Ticket {
+    /** The hit count. */
+    int hits = 0;
+}
