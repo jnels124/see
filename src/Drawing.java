5d9101efde1ee9b413b539107456d789938a6c7b
Jesse Nelson <jnels124@msudenver.edu>
2013-05-04 20:14:37 -0600

    Added Drawing.java from upstream

---
+++ src/Drawing.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+import java.util.List;
+
+public class Drawing {
+    /** The drawing date. */
+    Date date;
+    /** The numbers selected in the drawing. */
+    List< Integer > numbers;
+}
